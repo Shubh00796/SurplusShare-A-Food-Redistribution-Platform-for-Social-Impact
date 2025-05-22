1ebc4d150395031e5e0f1257593d8c9bbb184e5c
Shubh00796 <shubhamdongare9696@gmail.com>
2025-05-22 13:55:33 +0530

    Initial commit

---
+++ src/test/java/com/FoodWasteManagementSystem/FoodWasteManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FoodWasteManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodWasteManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
